4482c5ac88ec6d79207af625788d705ea762e4f9
Ivan Duran <idurandiaz@gmail.com>
2022-04-17 01:35:50 +0300

    Add golang linter

--- lua/setup/null-ls.lua
+++ lua/setup/null-ls.lua
@@ -14,9 +14,10 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		-- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-    formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
+		formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-		diagnostics.flake8
+		diagnostics.flake8,
+		diagnostics.golangci_lint,
 	},
 })
