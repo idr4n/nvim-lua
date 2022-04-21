397948fe171c57d7ed0f4ff437b4631e146d879e
Ivan Duran <idurandiaz@gmail.com>
2022-04-21 05:12:14 +0300

    Refinements in alpha to use telescope

--- lua/setup/alpha.lua
+++ lua/setup/alpha.lua
@@ -12,34 +12,34 @@ math.randomseed(os.time())
 -- end
 
 local function pick_color()
-  local colors = { "String", "Identifier", "Keyword", "Number" }
-  return colors[math.random(#colors)]
+	local colors = { "String", "Identifier", "Keyword", "Number" }
+	return colors[math.random(#colors)]
 end
 
 local function footer()
-  local total_plugins = #vim.tbl_keys(packer_plugins)
-  local datetime = os.date(" %d-%m-%Y   %H:%M:%S")
-  return datetime
-    .. "   "
-    .. total_plugins
-    .. " plugins"
-    .. "   v"
-    .. vim.version().major
-    .. "."
-    .. vim.version().minor
-    .. "."
-    .. vim.version().patch
+	local total_plugins = #vim.tbl_keys(packer_plugins)
+	local datetime = os.date(" %d-%m-%Y   %H:%M:%S")
+	return datetime
+		.. "   "
+		.. total_plugins
+		.. " plugins"
+		.. "   v"
+		.. vim.version().major
+		.. "."
+		.. vim.version().minor
+		.. "."
+		.. vim.version().patch
 end
 
 dashboard.section.header.val = {
-    "                                                     ",
-    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-    "                                                     ",
+	"                                                     ",
+	"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+	"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+	"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+	"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+	"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+	"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+	"                                                     ",
 }
 dashboard.section.header.opts.hl = pick_color()
 
@@ -53,16 +53,16 @@ dashboard.section.header.opts.hl = pick_color()
 -- }
 
 dashboard.section.buttons.val = {
-  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
 	-- dashboard.button("f", "  Find file", ":Files<cr>"),
-  -- dashboard.button("f", "  Find file", ":lua require('fzf-lua').files()<cr>"),
-  dashboard.button("f", "  Find file", ":Telescope find_files<cr>"),
-	dashboard.button("r", "  Recently used files", ":History<cr>"),
-  dashboard.button("t", "  Find text", ":Rg<cr>"),
-  -- dashboard.button("s", "  Open session", ":SearchSession <CR>"),
-  dashboard.button("s", "  Open directory", ":lua require('setup.fzf-lua.commands').workdirs()<CR>"),
-	dashboard.button("c", "  Configuration", ":Files ~/.config/nvim<CR>"),
-  dashboard.button("u", "  Update plugins", ":PackerSync<CR>"),
+	-- dashboard.button("f", "  Find file", ":lua require('fzf-lua').files()<cr>"),
+	dashboard.button("f", "  Find file", ":Telescope find_files<cr>"),
+	dashboard.button("r", "  Recently used files", ":Telescope oldfiles<cr>"),
+	dashboard.button("t", "  Find text", ":Telescope live_grep<cr>"),
+	-- dashboard.button("s", "  Open session", ":SearchSession <CR>"),
+	dashboard.button("s", "  Open directory", ":lua require('setup.fzf-lua.commands').workdirs()<CR>"),
+	dashboard.button("c", "  Configuration", ":lcd ~/.config/nvim | Telescope find_files<cr><CR>"),
+	dashboard.button("u", "  Update plugins", ":PackerSync<CR>"),
 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
