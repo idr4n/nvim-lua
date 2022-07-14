9b4452047dc699eafd91bfad6ec41df952bcda42
Ivan Duran <idurandiaz@gmail.com>
2022-07-14 16:26:10 +0300

    Using dirvish and netrw instead of nvim-tree

--- lua/options.lua
+++ lua/options.lua
@@ -69,3 +69,10 @@ vim.cmd([[
   au TextYankPost * silent! lua vim.highlight.on_yank {timeout=400}
   augroup END
 ]])
+
+-- Explorer (netrw)
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+vim.g.netrw_liststyle = 3
+vim.g.netrw_fastbrowse = 0
