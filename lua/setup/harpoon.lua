358d6789230fc19cfd58898fb87fbc92f9b6f661
Ivan Duran <idurandiaz@gmail.com>
2022-07-18 18:38:13 +0300

    Remove keybinding

--- lua/setup/harpoon.lua
+++ lua/setup/harpoon.lua
@@ -3,7 +3,7 @@
 vim.keymap.set("n", "<leader><tab>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>")
 vim.keymap.set("n", "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>")
 vim.keymap.set("n", "<c-s>", "<cmd>lua require('harpoon.ui').nav_next()<cr>")
-vim.keymap.set("n", "<c-x>", "<cmd>lua require('harpoon.ui').nav_prev()<cr>")
+-- vim.keymap.set("n", "<c-x>", "<cmd>lua require('harpoon.ui').nav_prev()<cr>")
 vim.keymap.set("n", "<M-u>", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>")
 vim.keymap.set("n", "<M-i>", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>")
 vim.keymap.set("n", "<M-o>", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>")
