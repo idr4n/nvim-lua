76eb9f5e261e81033a637f421d0bf129777a0001
Ivan Duran <idurandiaz@gmail.com>
2022-07-31 12:42:23 +0300

    Add some js snippets

--- snippets/javascript.lua
+++ snippets/javascript.lua
@@ -24,11 +24,35 @@ M.snips.cl = p({
 	dscr = "surrounds with console.log function",
 }, "console.log($TM_SELECTED_TEXT$1)")
 
+M.snips.af = p({
+	trig = "af",
+	name = "surrounds with arrow function",
+	dscr = "surrounds selection with an arrow function",
+}, "const ${1:name} = ($2) => {\n\t$TM_SELECTED_TEXT$3\n})")
+
+M.snips.aa = p({
+	trig = "aa",
+	name = "=> {|}",
+	dscr = "adds an arrow function",
+}, " => {$1}")
+
 -- autosnippets
 M.autosnips.cl = p({
 	trig = ";cl",
 	name = "Autosnippet - console.log(|)",
 	dscr = "surrounds with console.log function",
 }, "console.log($TM_SELECTED_TEXT$1)")
+
+M.autosnips.af = p({
+	trig = ";af",
+	name = "surrounds with arrow function",
+	dscr = "surrounds selection with an arrow function",
+}, "const ${1:name} = ($2) => {\n\t$TM_SELECTED_TEXT$3\n})")
+
+M.autosnips.aa = p({
+	trig = ";aa",
+	name = "=> {|}",
+	dscr = "adds an arrow function",
+}, " => {$1}")
 
 return getSnippetsList(M.snips), getSnippetsList(M.autosnips)
