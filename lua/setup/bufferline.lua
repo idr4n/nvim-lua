4648576d6f2c62ba70a8359e8dc6e76a005ebb31
Ivan Duran <idurandiaz@gmail.com>
2022-08-19 00:45:57 +0300

    Fix breaking change in bufferline

--- lua/setup/bufferline.lua
+++ lua/setup/bufferline.lua
@@ -6,11 +6,6 @@ require("bufferline").setup({
 		show_buffer_close_icons = false,
 		show_close_icon = false,
 		indicator_icon = "",
-		offsets = { { filetype = "neo-tree", text = "Neo-Tree" } },
-	},
-	highlights = {
-		buffer_selected = {
-			gui = "none",
-		},
+		offsets = { { filetype = "neo-tree", text = " Neo-Tree" } },
 	},
 })
