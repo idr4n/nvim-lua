f0c28beb258312853522146e9db3f37216cbc28f
Ivan Duran <idurandiaz@gmail.com>
2022-08-22 13:43:39 +0300

    Fix deprecated

--- lua/setup/bufferline.lua
+++ lua/setup/bufferline.lua
@@ -5,7 +5,7 @@ require("bufferline").setup({
 		-- color_icons = false,
 		show_buffer_close_icons = false,
 		show_close_icon = false,
-		indicator_icon = "",
+		indicator = { icon = "" },
 		offsets = { { filetype = "neo-tree", text = " Neo-Tree" } },
 	},
 })
