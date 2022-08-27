3f2f0d748f748949e07a4f55d24bffdfd5092260
Ivan Duran <idurandiaz@gmail.com>
2022-08-27 14:12:31 +0300

    Changes to toggleterm config

--- lua/setup/toggleterm.lua
+++ lua/setup/toggleterm.lua
@@ -6,7 +6,14 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	size = 25,
+	-- size = 25,
+	size = function(term)
+		if term.direction == "horizontal" then
+			return 17
+		elseif term.direction == "vertical" then
+			return vim.o.columns * 0.4
+		end
+	end,
 	open_mapping = [[<C-\>]],
 	hide_numbers = true,
 	shade_filetypes = {},
@@ -15,8 +22,8 @@ toggleterm.setup({
 	start_in_insert = true,
 	insert_mappings = false,
 	persist_size = true,
-	direction = "float",
-	-- direction = "horizontal",
+	-- direction = "float",
+	direction = "horizontal",
 	close_on_exit = true,
 	shell = vim.o.shell,
 	float_opts = {
@@ -77,3 +84,16 @@ vim.api.nvim_set_keymap("n", ",l", ":LF<cr>", { noremap = true, silent = true })
 -- 	":TermExec cmd='clear && ~/scripts/code_run \"%\"'<cr>",
 -- 	{ noremap = true, silent = true }
 -- )
+
+function _G.set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+	vim.keymap.set("t", "<C-;>", [[<Cmd>wincmd l<CR>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
