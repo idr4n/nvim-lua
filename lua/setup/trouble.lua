3e55460f71c2544e26665ea666c6014c4d49f6a7
Ivan Duran <idurandiaz@gmail.com>
2022-08-30 10:59:52 +0300

    Remap gd back to telescope go to definition

--- lua/setup/trouble.lua
+++ lua/setup/trouble.lua
@@ -27,4 +27,4 @@ vim.api.nvim_set_keymap(
 vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
 vim.api.nvim_set_keymap("n", "gr", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap("n", "gd", "<cmd>TroubleToggle lsp_definitions<cr>", { silent = true, noremap = true })
+-- vim.api.nvim_set_keymap("n", "gd", "<cmd>TroubleToggle lsp_definitions<cr>", { silent = true, noremap = true })
