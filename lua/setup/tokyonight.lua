e5c1f832d7aaf7d5b7dcae770d493b9bd301dd2d
Ivan Duran <idurandiaz@gmail.com>
2022-09-06 19:45:57 +0300

    Fix break changes in tokyonight

--- lua/setup/tokyonight.lua
+++ lua/setup/tokyonight.lua
@@ -1,24 +1,17 @@
 -- 'folke/tokyonight.nvim'
 
--- Example config in Lua
--- vim.g.tokyonight_style = "storm"
--- vim.g.tokyonight_style = "night"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_transparent = true
--- vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
--- vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
--- vim.g.tokyonight_colors = { bg_highlight = "#232431" }
+require("tokyonight").setup({
+	transparent = true,
+	styles = {
+		functions = "italic",
+	},
+})
 
 -- Load the colorscheme
 local t = os.date("*t").hour + os.date("*t").min / 60
 
 if t >= 8 and t < 18 then
-	vim.g.tokyonight_style = "storm"
-	-- vim.cmd("colorscheme tokyonight")
+	vim.cmd("colorscheme tokyonight-storm")
 else
-	vim.g.tokyonight_style = "night"
-	vim.cmd("colorscheme tokyonight")
+	vim.cmd("colorscheme tokyonight-night")
 end
--- vim.cmd("colorscheme tokyonight")
