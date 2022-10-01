8729c437930a2017e821fe25b6f84b1818053ca8
Ivan Duran <idurandiaz@gmail.com>
2022-10-01 22:11:03 +0300

    Catppuccin modifications to match tokyo-night

--- lua/setup/catppuccin.lua
+++ lua/setup/catppuccin.lua
@@ -1,9 +1,14 @@
 -- "catppuccin/nvim"
 
+local ucolors = require("catppuccin.utils.colors")
+local cp = require("catppuccin.palettes").get_palette()
+
 require("catppuccin").setup({
 	transparent_background = true,
 	styles = {
 		functions = { "italic" },
+		keywords = { "italic" },
+		conditionals = {},
 	},
 	integrations = {
 		native_lsp = {
@@ -15,11 +20,26 @@ require("catppuccin").setup({
 			},
 		},
 	},
+	custom_highlights = {
+		CursorLine = {
+			bg = ucolors.vary_color(
+				{ latte = ucolors.lighten(cp.mantle, 0.70, cp.base) },
+				ucolors.darken(cp.surface0, 0.70, cp.base)
+			),
+		},
+		TSParameter = { fg = cp.maroon, style = {} },
+		["@parameter"] = { fg = cp.maroon, style = {} },
+		TSInclude = { fg = cp.mauve, style = {} },
+		["@include"] = { fg = cp.mauve, style = {} },
+		["@namespace"] = { fg = cp.blue, style = {} },
+		TSNamespace = { fg = cp.blue, style = {} },
+		VertSplit = { fg = "#15161e" },
+	},
 	compile = {
 		-- :CatppuccinCompile " Create/update the compile file
 		-- :CatppuccinClean " Delete compiled file
 		-- :CatppuccinStatus " Compile status
-		enabled = true,
+		enabled = false,
 		path = vim.fn.stdpath("cache") .. "/catppuccin",
 	},
 })
@@ -33,4 +53,4 @@ else
 	vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
 end
 
--- vim.cmd([[colorscheme catppuccin]])
+vim.cmd([[colorscheme catppuccin]])
