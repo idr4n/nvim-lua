a357e6dd4dd9283dcdf069c08b75dfbc1153b288
Ivan Duran <idurandiaz@gmail.com>
2022-10-08 10:39:07 +0300

    Change depracated formatting callback

--- lua/commands.lua
+++ lua/commands.lua
@@ -94,7 +94,9 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_augroup("LspFormatting", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = { "*.go", "*.lua" },
-	callback = vim.lsp.buf.formatting_sync,
+	callback = function()
+		vim.lsp.buf.format()
+	end,
 	group = "LspFormatting",
 })
 
