0776f8f0ab053125cb768bb7f559bdd4ffafc69f
Ivan Duran <idurandiaz@gmail.com>
2023-02-04 11:11:44 +0300

    Tried barbecue but prefer simple with incline

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -3,18 +3,14 @@ return {
 	name = "barbecue",
 	enabled = false,
 	event = "BufReadPre",
-	version = "*",
 	dependencies = {
 		"SmiteshP/nvim-navic",
 		"nvim-tree/nvim-web-devicons", -- optional dependency
 	},
 	opts = {
 		show_navic = true,
-		show_dirname = false,
+		show_dirname = true,
 		show_modified = true,
-		-- modifiers = {
-		-- 	dirname = ":s?.*??",
-		-- },
 		theme = {
 			basename = { fg = "#9D7CD8", bold = true },
 		},
