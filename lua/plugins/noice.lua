8343b973d08ee229d8c9a00b1b349b667a438654
Ivan Duran <idurandiaz@gmail.com>
2023-02-20 10:09:53 +0300

    Add some noice plugin keybindings

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -18,6 +18,12 @@ return {
         dependencies = {
             "rcarriga/nvim-notify",
         },
+        -- stylua: ignore
+        keys = {
+            { "<leader>nl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+            { "<leader>nh", function() require("noice").cmd("history") end, desc = "Noice History" },
+            { "<leader>na", function() require("noice").cmd("all") end, desc = "Noice All" },
+        },
         opts = {
             lsp = {
                 override = {
@@ -30,6 +36,7 @@ return {
             },
             presets = {
                 long_message_to_split = true,
+                command_palette = true,
             },
         },
     },
