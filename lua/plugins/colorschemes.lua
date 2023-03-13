2d86d252f8c62d65180dc6ec1d576a399ca0f92d
Ivan Duran <idurandiaz@gmail.com>
2023-03-13 08:09:50 +0300

    Oxocarbon LspReferenceText fix

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -272,7 +272,69 @@ return {
             else
                 vim.o.background = "dark"
             end
+
+            local _local_1_ = require("oxocarbon.colorutils")
+            local blend_hex = _local_1_["blend-hex"]
+            if vim.g.colors_name then
+                vim.cmd.hi("clear")
+            end
+            vim.g["colors_name"] = "oxocarbon"
+            vim.o["termguicolors"] = true
+            local base00 = "#161616"
+            local base06 = "#ffffff"
+            local base09 = "#78a9ff"
+
+            local oxocarbon = (
+                (
+                    (vim.o.background == "dark")
+                    and {
+                        base00 = base00,
+                        base01 = blend_hex(base00, base06, 0.085),
+                        base02 = blend_hex(base00, base06, 0.18),
+                        base03 = blend_hex(base00, base06, 0.3),
+                        base04 = blend_hex(base00, base06, 0.82),
+                        base05 = blend_hex(base00, base06, 0.95),
+                        base06 = base06,
+                        base07 = "#08bdba",
+                        base08 = "#3ddbd9",
+                        base09 = base09,
+                        base10 = "#ee5396",
+                        base11 = "#33b1ff",
+                        base12 = "#ff7eb6",
+                        base13 = "#42be65",
+                        base14 = "#be95ff",
+                        base15 = "#82cfff",
+                        blend = "#131313",
+                        none = "NONE",
+                    }
+                )
+                or {
+                    base00 = base06,
+                    base01 = blend_hex(base00, base06, 0.95),
+                    base02 = blend_hex(base00, base06, 0.82),
+                    base03 = blend_hex(base00, base06, 0.80),
+                    base04 = "#37474F",
+                    base05 = "#90A4AE",
+                    base06 = "#525252",
+                    base07 = "#08bdba",
+                    base08 = "#ff7eb6",
+                    base09 = "#ee5396",
+                    base10 = "#FF6F00",
+                    base11 = "#0f62fe",
+                    base12 = "#673AB7",
+                    base13 = "#42be65",
+                    base14 = "#be95ff",
+                    base15 = "#FFAB91",
+                    blend = "#FAFAFA",
+                    none = "NONE",
+                }
+            )
+
             vim.cmd.colorscheme("oxocarbon")
+            vim.api.nvim_set_hl(0, "LspReferenceText", { fg = oxocarbon.none, bg = oxocarbon.base03 })
+            vim.api.nvim_set_hl(0, "LspReferenceRead", { fg = oxocarbon.none, bg = oxocarbon.base03 })
+            vim.api.nvim_set_hl(0, "LspReferenceWrite", { fg = oxocarbon.none, bg = oxocarbon.base03 })
+            vim.api.nvim_set_hl(0, "Comment", { fg = oxocarbon.base03, bg = oxocarbon.none, italic = true })
         end,
     },
     --: }}}
