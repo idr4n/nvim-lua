df5e0b2af4a406454b1d85f867b106a08e8e12d4
Ivan Duran <idurandiaz@gmail.com>
2023-03-16 00:02:36 +0300

    Noice adjustments

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -76,8 +76,8 @@ return {
                     ["vim.lsp.util.stylize_markdown"] = true,
                     ["cmp.entry.get_documentation"] = true,
                 },
-                hover = { enabled = false },
-                signature = { enabled = false },
+                hover = { enabled = true },
+                signature = { enabled = true },
                 progress = { enabled = false },
             },
             cmdline = {
