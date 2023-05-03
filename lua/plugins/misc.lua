14a581710891edd6f598e32d277fb0dcd3e98929
Ivan Duran <idurandiaz@gmail.com>
2023-05-03 10:43:30 +0300

    NoNeckPain plugin added

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -152,4 +152,27 @@ return {
         },
     },
     --: }}}
+
+    --: no-neck-pain (another zen-mode plugin) {{{
+    {
+        "shortcuts/no-neck-pain.nvim",
+        cmd = { "NoNeckPain" },
+        keys = {
+            {
+                "<leader>tz",
+                function()
+                    vim.cmd([[
+                        NoNeckPain
+                        set invnumber
+                        set invrelativenumber
+                    ]])
+                end,
+                desc = "Zen-mode (No-neck-pain)",
+            },
+        },
+        opts = {
+            buffers = { wo = { number = false, relativenumber = false } },
+        },
+    },
+    --: }}}
 }
