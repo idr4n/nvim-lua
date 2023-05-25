db27e6d82a7b138d46fb9794dbbbdfe1b92655fb
Ivan Duran <idurandiaz@gmail.com>
2023-05-25 23:06:56 +0300

    Update some nerd font icons

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -22,16 +22,16 @@ return {
         }
 
         dashboard.section.buttons.val = {
-            dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-            -- dashboard.button("f", "  Find file", ":Files<cr>"),
-            dashboard.button("f", "  Find file", ":lua require('fzf-lua').files()<cr>"),
+            dashboard.button("e", "󰈔  New file", ":ene <BAR> startinsert <CR>"),
+            -- dashboard.button("f", "󰱼  Find file", ":Files<cr>"),
+            dashboard.button("f", "󰱼  Find file", ":lua require('fzf-lua').files()<cr>"),
             -- dashboard.button(
             --     "f",
             --     "  Find file",
             --     ":lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>"
             -- ),
-            dashboard.button("r", "  Recently used files", ":Telescope oldfiles<cr>"),
-            dashboard.button("t", "  Find text", ":Rg<cr>"),
+            dashboard.button("r", "󰄉  Recently used files", ":Telescope oldfiles<cr>"),
+            dashboard.button("t", "󰺮  Find text", ":Rg<cr>"),
             -- dashboard.button("s", "  Open session", ":SearchSession <CR>"),
             dashboard.button(
                 "s",
@@ -43,7 +43,7 @@ return {
             dashboard.button("c", "  Configuration", "<cmd>lcd ~/.config/nvim<cr>"),
             dashboard.button("d", "  Dotfiles", "<cmd>lcd ~/dotfiles<cr>"),
             dashboard.button("u", "  Update plugins", ":Lazy<CR>"),
-            dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+            dashboard.button("q", "󰅚  Quit Neovim", ":qa<CR>"),
         }
 
         dashboard.section.footer.opts.hl = "Type"
