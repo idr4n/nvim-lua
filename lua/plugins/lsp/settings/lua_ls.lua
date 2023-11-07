dd6ac3e55d00cf6c1baf4025a484216b35555657
Ivan Duran <idurandiaz@gmail.com>
2023-11-07 19:45:16 +0300

    Fix checkThirdParty setting

--- lua/plugins/lsp/settings/lua_ls.lua
+++ lua/plugins/lsp/settings/lua_ls.lua
@@ -9,7 +9,7 @@ return {
                     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
                     [vim.fn.stdpath("config") .. "/lua"] = true,
                 },
-                checkThirdParty = false,
+                checkThirdParty = "Disable",
             },
         },
     },
