99774b14ac7604fab4048d1537833fa076cf7a57
Ivan Duran <idurandiaz@gmail.com>
2023-12-31 19:15:11 +0300

    Add catppuccin

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -166,6 +166,51 @@ return {
         end,
     },
     --: }}}
+
+    --: Catppuccin {{{
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        opts = function()
+            local ucolors = require("catppuccin.utils.colors")
+            local cp = require("catppuccin.palettes").get_palette()
+            local acc_bg = "#1E2030"
+            -- local acc_bg = cp.none
+            -- local win_sep = "#24273A"
+            return {
+                -- transparent_background = true,
+                styles = {
+                    functions = { "italic" },
+                    keywords = { "italic" },
+                    conditionals = {},
+                },
+                integrations = {
+                    native_lsp = {
+                        underlines = {
+                            errors = { "undercurl" },
+                            hints = { "undercurl" },
+                            warnings = { "undercurl" },
+                            information = { "undercurl" },
+                        },
+                    },
+                },
+                custom_highlights = {
+                    TSParameter = { fg = cp.maroon, style = {} },
+                    ["@parameter"] = { fg = cp.maroon, style = {} },
+                    TSInclude = { fg = cp.mauve, style = {} },
+                    ["@include"] = { fg = cp.mauve, style = {} },
+                    ["@namespace"] = { fg = cp.blue, style = {} },
+                    TSNamespace = { fg = cp.blue, style = {} },
+                    Folded = { bg = "NONE" },
+                    StatusLine = { bg = acc_bg },
+                    NeoTreeNormal = { bg = acc_bg },
+                    NeoTreeNormalNC = { bg = acc_bg },
+                    NotifyBackground = { bg = "#000000" },
+                },
+            }
+        end,
+    },
+    --: }}}
 }
 
 -- Monokai-pro palette
