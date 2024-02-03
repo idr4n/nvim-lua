6e8b6b57965efbbe0d5b1b91df2094fadd8029fa
Ivan Duran <idurandiaz@gmail.com>
2024-02-03 20:47:11 +0300

    Add pre-save function to sessions manager

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -838,7 +838,23 @@ local plugins = {
   {
     "folke/persistence.nvim",
     event = "BufReadPre",
-    opts = { options = vim.opt.sessionoptions:get() },
+    opts = function()
+      local function pre_save()
+        -- remove buffers whose files are located outside of cwd
+        local cwd = vim.fn.getcwd() .. "/"
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          local bufpath = vim.api.nvim_buf_get_name(buf) .. "/"
+          if not bufpath:match("^" .. vim.pesc(cwd)) then
+            vim.api.nvim_buf_delete(buf, {})
+          end
+        end
+      end
+
+      return {
+        options = vim.opt.sessionoptions:get(),
+        pre_save = pre_save,
+      }
+    end,
         -- stylua: ignore
         keys = {
             { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
