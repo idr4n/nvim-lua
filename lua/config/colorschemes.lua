b75713b2e68d8b4c58bc553edce41816e16d3204
Ivan Duran <idurandiaz@gmail.com>
2024-04-02 02:50:14 +0300

    Adjust github-nvim-theme settings

--- lua/config/colorschemes.lua
+++ lua/config/colorschemes.lua
@@ -63,41 +63,4 @@ M.wind = {
   end,
 }
 
-M.github = {
-  setup = function()
-    vim.cmd("colorscheme github_light")
-    if vim.g.colors_name == "github_light" then
-      -- local normal_hl = vim.api.nvim_get_hl(0, { name = "Normal" })
-      -- vim.api.nvim_set_hl(0, "Normal", vim.tbl_extend("force", normal_hl, { bg = "#FAFAFA" }))
-      vim.api.nvim_set_hl(0, "Comment", { link = "Comment", fg = "#6A737D" })
-      vim.api.nvim_set_hl(0, "Delimiter", { link = "Delimiter", fg = "#24292F" }) -- for punctuation ',' '.', etc
-      vim.api.nvim_set_hl(0, "LineNr", { fg = "#BFC0C1" })
-      vim.api.nvim_set_hl(0, "CursorLine", { bg = "#F6F8FA" })
-      vim.api.nvim_set_hl(0, "StatusLine", { bg = "#F1F3F5" })
-      vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { fg = "#FFFFFF" })
-      vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#FFFFFF" })
-      vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "#24292F" })
-      vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "#F3F5F7" })
-      vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#F3F5F7", bg = "#F3F5F7" })
-      vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "#EDEFF1" })
-      vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#F3F5F7", bg = "#F3F5F7" })
-      vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = "#F3F5F7", bg = "#18654B" })
-      vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#F3F5F7", bg = "#F3F5F7" })
-      vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "#EDEFF1" })
-      vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = "#F3F5F7", bg = "#DE2C2E" })
-      vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = "#DE2C2E", bg = "#EDEFF1" })
-      vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#EDEFF1", bg = "#EDEFF1" })
-
-      -- -- if changing background to something else such as #FAFAFA
-      -- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#EFF0F2" })
-      -- vim.api.nvim_set_hl(0, "StatusLine", { bg = "#E7EAF0" })
-      -- vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "#F3F4F6" })
-      -- vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "#F3F4F6" })
-      -- vim.api.nvim_set_hl(0, "NeoTreeCursorLine", { bg = "#E2E5EA" })
-      -- vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "#F3F4F6" })
-      -- vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { fg = "#F3F4F6", bg = "#F3F4F6" })
-    end
-  end,
-}
-
 return M
--- lua/plugins/github-nvim-theme.lua
+++ lua/plugins/github-nvim-theme.lua
@@ -12,6 +12,28 @@ return {
           },
         },
       },
+      groups = {
+        github_light = {
+          StatusLine = { bg = "#F1F3F5" },
+          Comment = { fg = "#6A737D" },
+          Delimiter = { fg = "#24292F" }, -- for punctuation ',' '.', etc
+          LineNr = { fg = "#BFC0C1" },
+          CursorLine = { bg = "#F6F8FA" },
+          NeoTreeWinSeparator = { fg = "#FFFFFF" },
+          TreesitterContext = { bg = "#FFFFFF" },
+          TreesitterContextBottom = { underline = true, sp = "#24292F" },
+          TelescopeNormal = { bg = "#F3F5F7" },
+          TelescopeBorder = { fg = "#F3F5F7", bg = "#F3F5F7" },
+          TelescopeSelection = { bg = "#EDEFF1" },
+          TelescopeResultsBorder = { fg = "#F3F5F7", bg = "#F3F5F7" },
+          TelescopePreviewTitle = { fg = "#F3F5F7", bg = "#18654B" },
+          TelescopePreviewBorder = { fg = "#F3F5F7", bg = "#F3F5F7" },
+          TelescopePromptNormal = { bg = "#EDEFF1" },
+          TelescopePromptTitle = { fg = "#F3F5F7", bg = "#DE2C2E" },
+          TelescopePromptPrefix = { fg = "#DE2C2E", bg = "#EDEFF1" },
+          TelescopePromptBorder = { fg = "#EDEFF1", bg = "#EDEFF1" },
+        },
+      },
     })
   end,
 }
