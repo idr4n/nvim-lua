0ff4bb535379398f2893c8dd5340d642212726a0
Ivan Duran <idurandiaz@gmail.com>
2024-04-20 10:54:25 +0300

    Add markdown-preview customization

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -5,7 +5,8 @@ return {
   build = function()
     vim.fn["mkdp#util#install"]()
   end,
-  -- config = function()
-  --   vim.g.mkdp_browser = "Vivaldi"
-  -- end,
+  config = function()
+    -- vim.g.mkdp_browser = "Vivaldi"
+    vim.g.mkdp_highlight_css = vim.fn.expand("~/md-preview.css")
+  end,
 }
