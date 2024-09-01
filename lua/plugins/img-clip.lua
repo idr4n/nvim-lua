ccecde3056789bd7e9b02a2975d9a5e6906c006f
Ivan Duran <idurandiaz@gmail.com>
2024-09-01 22:39:28 +0300

    Add img-clip plugin

---
+++ lua/plugins/img-clip.lua
@@ -0,0 +1,16 @@
+return {
+  "HakonHarnes/img-clip.nvim",
+  cmd = { "PasteImage", "ImgClipDebug" },
+  opts = {
+    default = {
+      embed_image_as_base64 = false,
+      prompt_for_file_name = false,
+      drag_and_drop = {
+        insert_mode = true,
+      },
+    },
+  },
+  keys = {
+    { "<leader>pi", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+  },
+}
