cb26c1435ad7fe43be507fa562f8cdfe4dc7b903
Ivan Duran <idurandiaz@gmail.com>
2024-09-10 12:07:07 +0300

    Add aerial

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,51 @@
+return {
+  "stevearc/aerial.nvim",
+  -- Optional dependencies
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+    { "<leader>ta", "<cmd>AerialToggle!<cr>", desc = "Aerial Toggle" },
+  },
+  opts = {
+    attach_mode = "global",
+    backends = { "lsp", "markdown", "asciidoc", "man" },
+    show_guides = true,
+    layout = {
+      resize_to_content = false,
+      max_width = { 40, 0.3 },
+      width = 32,
+      default_direction = "prefer_left",
+      win_opts = {
+        signcolumn = "yes",
+        statuscolumn = " ",
+      },
+    },
+    icons = require("utils").lazyvim_icons,
+    filter_kind = {
+      "Class",
+      "Constructor",
+      "Enum",
+      "Field",
+      "Function",
+      "Interface",
+      "Method",
+      "Module",
+      "Namespace",
+      "Object",
+      "Package",
+      "Property",
+      "Struct",
+      "Trait",
+      "Variable",
+    },
+      -- stylua: ignore
+      guides = {
+        mid_item   = "├╴",
+        last_item  = "└╴",
+        nested_top = "│ ",
+        whitespace = "  ",
+      },
+  },
+}
