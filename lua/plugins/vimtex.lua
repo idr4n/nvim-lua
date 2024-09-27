f0c919245130ab8825f363ff4e4e01cdab5e9ac5
Ivan Duran <idurandiaz@gmail.com>
2024-09-27 10:45:01 +0300

    Vimtex preview method

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,7 +3,7 @@ return {
   -- enabled = false,
   ft = { "tex" },
   config = function()
-    vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_view_method = vim.fn.has("mac") == 1 and "skim" or "zathura"
     vim.g.vimtex_compiler_silent = 1
     -- vim.g.vimtex_syntax_enabled = 0
   end,
