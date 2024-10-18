d4578b295b8fd447a1d82f8945658491ff74ede0
Ivan Duran <idurandiaz@gmail.com>
2024-10-18 22:22:30 +0300

    Remove vimtex autocmd

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,16 +6,5 @@ return {
     vim.g.vimtex_compiler_silent = 1
     vim.g.vimtex_syntax_enabled = 0
     vim.g.vimtex_compiler_clean_paths = { "*.gz" }
-
-    if vim.fn.has("linux") then
-      local au_group = vim.api.nvim_create_augroup("idr4n/vimtex_events", {})
-
-      -- Focus the terminal after inverse search
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "VimtexEventViewReverse",
-        group = au_group,
-        command = "call b:vimtex.viewer.xdo_focus_vim()",
-      })
-    end
   end,
 }
