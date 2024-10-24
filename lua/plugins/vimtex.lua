2cc4d29cd29208e84d95ae7a86bc9fb1fb5039a7
Ivan Duran <idurandiaz@gmail.com>
2024-10-24 11:42:15 +0300

    Cleanup config

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,6 +5,5 @@ return {
     vim.g.vimtex_view_method = vim.fn.has("mac") == 1 and "sioyek" or "zathura"
     vim.g.vimtex_compiler_silent = 1
     vim.g.vimtex_syntax_enabled = 0
-    vim.g.vimtex_compiler_clean_paths = { "*.gz" }
   end,
 }
