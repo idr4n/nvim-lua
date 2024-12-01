2631b211b1ab5c1fb5b9a036fcb35629c709e988
Ivan Duran <idurandiaz@gmail.com>
2024-12-01 21:37:31 +0300

    Add gruvbox-material colorscheme

---
+++ lua/plugins/gruvbox-material.lua
@@ -0,0 +1,64 @@
+return {
+  "sainnhe/gruvbox-material",
+  priority = 1000,
+  config = function()
+    vim.g.gruvbox_material_transparent_background = 0
+    -- vim.g.gruvbox_material_foreground = "mix"
+    -- vim.g.gruvbox_material_foreground = "original"
+    vim.g.gruvbox_material_background = "hard"
+    vim.g.gruvbox_material_ui_contrast = "high"
+    -- vim.g.gruvbox_material_float_style = "dim"
+    -- vim.g.gruvbox_material_statusline_style = "original"
+
+    -- vim.g.gruvbox_material_better_performance = 1
+  end,
+}
+
+-- Gruvbox material hard
+-- \ 'fg0':              ['#d4be98',   '223'],
+-- \ 'fg1':              ['#ddc7a1',   '223'],
+-- \ 'red':              ['#ea6962',   '167'],
+-- \ 'orange':           ['#e78a4e',   '208'],
+-- \ 'yellow':           ['#d8a657',   '214'],
+-- \ 'green':            ['#a9b665',   '142'],
+-- \ 'aqua':             ['#89b482',   '108'],
+-- \ 'blue':             ['#7daea3',   '109'],
+-- \ 'purple':           ['#d3869b',   '175'],
+-- \ 'bg_red':           ['#ea6962',   '167'],
+-- \ 'bg_green':         ['#a9b665',   '142'],
+-- \ 'bg_yellow':        ['#d8a657',   '214']
+-- \ 'grey0':            ['#7c6f64',   '243'],
+-- \ 'grey1':            ['#928374',   '245'],
+-- \ 'grey2':            ['#a89984',   '246'],
+-- \ 'bg_dim':           ['#141617',   '232'],
+-- \ 'bg0':              ['#1d2021',   '234'],
+-- \ 'bg1':              ['#282828',   '235'],
+-- \ 'bg2':              ['#282828',   '235'],
+-- \ 'bg3':              ['#3c3836',   '237'],
+-- \ 'bg4':              ['#3c3836',   '237'],
+-- \ 'bg5':              ['#504945',   '239'],
+-- \ 'bg_statusline1':   ['#282828',   '235'],
+-- \ 'bg_statusline2':   ['#32302f',   '235'],
+-- \ 'bg_statusline3':   ['#504945',   '239'],
+-- \ 'bg_diff_green':    ['#32361a',   '22'],
+-- \ 'bg_visual_green':  ['#333e34',   '22'],
+-- \ 'bg_diff_red':      ['#3c1f1e',   '52'],
+-- \ 'bg_visual_red':    ['#442e2d',   '52'],
+-- \ 'bg_diff_blue':     ['#0d3138',   '17'],
+-- \ 'bg_visual_blue':   ['#2e3b3b',   '17'],
+-- \ 'bg_visual_yellow': ['#473c29',   '94'],
+-- \ 'bg_current_word':  ['#32302f',   '236']
+
+-- Gruvbox material mix
+-- \ 'fg0':              ['#e2cca9',   '223'],
+-- \ 'fg1':              ['#e2cca9',   '223'],
+-- \ 'red':              ['#f2594b',   '167'],
+-- \ 'orange':           ['#f28534',   '208'],
+-- \ 'yellow':           ['#e9b143',   '214'],
+-- \ 'green':            ['#b0b846',   '142'],
+-- \ 'aqua':             ['#8bba7f',   '108'],
+-- \ 'blue':             ['#80aa9e',   '109'],
+-- \ 'purple':           ['#d3869b',   '175'],
+-- \ 'bg_red':           ['#db4740',   '167'],
+-- \ 'bg_green':         ['#b0b846',   '142'],
+-- \ 'bg_yellow':        ['#e9b143',   '214']
