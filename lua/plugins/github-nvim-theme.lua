5a2b47724de62c0ba82287f8d53b3bb4a8647b77
Ivan Duran <idurandiaz@gmail.com>
2024-12-02 23:06:57 +0300

    Update github-nvim-theme config

--- lua/plugins/github-nvim-theme.lua
+++ lua/plugins/github-nvim-theme.lua
@@ -2,12 +2,11 @@ return {
   "projekt0n/github-nvim-theme",
   lazy = true,
   config = function()
-    -- local bg_ln = "#F6F8FA"
     local bg = "#F4F4F4"
-    local bg_accent = "#EFEFEF"
     local bg_accent1 = "#EBEBEB"
     local bg_accent2 = "#E9E9E9"
-    local accent_purple = "#9A5BFF"
+    local accent_purple1 = "#6639BA"
+    local accent_purple2 = "#E8E0F4"
     local fg_light = "#7A838E"
     require("github-theme").setup({
       options = {
@@ -34,34 +33,36 @@ return {
         github_light = {
           Normal = { bg = bg },
           NormalNC = { bg = bg },
-          StatusLine = { bg = "#F1F3F5" },
+          StatusLine = { fg = "palette.fg.subtle", bg = bg },
           Comment = { fg = "#6A737D" },
           NonText = { fg = "#EDEFF1" },
           Delimiter = { fg = "#24292F" }, -- for punctuation ',' '.', etc
           LineNr = { fg = "#BABBBD" },
-          FloatBorder = { fg = "#1F2328" },
+          -- FloatBorder = { fg = "#1F2328" },
+          FloatBorder = { fg = accent_purple1 },
           NormalFloat = { bg = "None" },
           IblScope = { fg = "palette.fg.muted" },
           DiagnosticUnnecessary = { fg = "None" },
           -- CursorLine = { bg = "#F6F8FA" },
-          CursorLineNr = { fg = accent_purple, style = "bold" },
+          CursorLineNr = { fg = accent_purple1, style = "bold" },
           EndOfBuffer = { fg = "#EDEFF1" },
-          NeoTreeWinSeparator = { fg = "#FFFFFF" },
-          NeoTreeCursorLine = { bg = "#EDEFF1" },
+          NeoTreeWinSeparator = { fg = bg },
+          NeoTreeCursorLine = { bg = bg_accent2 },
           NvimTreeNormal = { bg = bg },
           NvimTreeNormalNC = { bg = bg },
           NvimTreeNormalFloat = { bg = bg },
           NvimTreeNormalFloatNC = { bg = bg },
-          NvimTreeNormalFloatBorder = { fg = accent_purple },
-          NvimTreeEndOfBuffer = { fg = "#F6F8FA" },
-          NvimTreeCursorLineNr = { bg = "#EDEFF1" },
-          NvimTreeIndentMarker = { fg = "#EDEFF1" },
+          NvimTreeNormalFloatBorder = { fg = accent_purple1 },
+          NvimTreeEndOfBuffer = { fg = bg_accent2 },
+          NvimTreeCursorLineNr = { bg = bg_accent2 },
+          NvimTreeIndentMarker = { fg = bg_accent2 },
           NvimTreeFolderArrowClosed = { fg = "#57606A" },
           NvimTreeFolderArrowOpen = { fg = "#57606A" },
           TreesitterContext = { bg = bg },
-          TreesitterContextBottom = { style = "underline", sp = accent_purple },
-          TelescopeSelection = { bg = "#F3F5F7" },
+          TreesitterContextBottom = { style = "underline", sp = accent_purple1 },
+          TelescopeSelection = { bg = bg_accent2 },
           TelescopeSelectionCaret = { fg = "#FF87D7" },
+          TelescopeBorder = { fg = accent_purple1, bg = "none" },
           TelescopeMatching = { fg = "#FF87D7" },
           MiniTablineFill = { bg = bg_accent1 },
           MiniTablineCurrent = { fg = "palette.gray", bg = bg },
