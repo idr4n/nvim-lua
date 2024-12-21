a8bb73085e6ed29b76e935dd3bc320fcb2540a0f
Ivan Duran <idurandiaz@gmail.com>
2024-12-22 01:02:22 +0300

    Actually add blink

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,85 @@
+return {
+  "saghen/blink.cmp",
+  event = { "InsertEnter", "BufReadPost" },
+  dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
+  version = "v0.*",
+  build = "cargo build --release",
+
+  opts = {
+    snippets = {
+      expand = function(snippet)
+        require("luasnip").lsp_expand(snippet)
+      end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require("luasnip").jumpable(filter.direction)
+        end
+        return require("luasnip").in_snippet()
+      end,
+      jump = function(direction)
+        require("luasnip").jump(direction)
+      end,
+    },
+    sources = {
+      default = { "lsp", "path", "luasnip", "buffer" },
+    },
+
+    completion = {
+      accept = {
+        -- experimental auto-brackets support
+        auto_brackets = {
+          enabled = true,
+        },
+      },
+      list = {
+        selection = "auto_insert",
+      },
+      menu = {
+        border = "rounded",
+        draw = {
+          gap = 2,
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                if require("blink.cmp.completion.windows.render.tailwind").get_hex_color(ctx.item) then
+                  return "󱓻"
+                end
+                return ctx.kind_icon .. ctx.icon_gap
+              end,
+            },
+          },
+        },
+        winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+        window = {
+          border = "rounded",
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+        },
+      },
+    },
+
+    keymap = {
+      preset = "enter",
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<C-y>"] = { "select_and_accept" },
+
+      ["<C-j>"] = { "snippet_forward", "fallback" },
+      ["<C-k>"] = { "snippet_backward", "fallback" },
+
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+    },
+  },
+  opts_extend = { "sources.default" },
+}
