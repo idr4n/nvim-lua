255d730549ed2cf72b8790e157fdf6ce9256cec0
Ivan Duran <idurandiaz@gmail.com>
2025-01-03 01:19:42 +0300

    Update ruff lsp name

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -127,7 +127,7 @@ M.on_attach = function(client, bufnr)
     end
   end
 
-  local clientsNoHover = { "tailwindcss", "cssmodules_ls", "ruff_lsp" }
+  local clientsNoHover = { "tailwindcss", "cssmodules_ls", "ruff" }
   for _, v in ipairs(clientsNoHover) do
     if client.name == v then
       client.server_capabilities.hoverProvider = false
