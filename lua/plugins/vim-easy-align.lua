3551535ea679aecc59258d566f9b6438b6e92e05
Ivan Duran <idurandiaz@gmail.com>
2025-01-09 10:30:18 +0300

    Add vim-easy-align

---
+++ lua/plugins/vim-easy-align.lua
@@ -0,0 +1,7 @@
+return {
+  "junegunn/vim-easy-align",
+  keys = {
+    { "ga", "<Plug>(EasyAlign)", mode = "x", desc = "EasyAlign" },
+    { "<leader>pa", ":EasyAlign = l5", mode = "v", desc = "EasyAlign" },
+  },
+}
