9a826cd16c5142c63ec6d08adea5b47de750243b
Ivan Duran <idurandiaz@gmail.com>
2025-05-25 11:38:08 +0300

    chore: remove unused plugins

--- lua/plugins/gruvbox.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "morhetz/gruvbox",
-  config = function()
-    vim.g.gruvbox_contrast_dark = "hard"
-    vim.g.gruvbox_sign_column = "bg0"
-    vim.g.gruvbox_invert_selection = 0
-  end,
-}
