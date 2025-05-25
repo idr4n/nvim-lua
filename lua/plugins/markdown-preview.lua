ae6b0699d1d5ff9e9713fa96958128615f97b692
Ivan Duran <idurandiaz@gmail.com>
2025-05-26 01:52:49 +0300

    fix: issue in markdown-preview's build

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,9 +2,8 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
+  -- Build issue discussed here: https://github.com/iamcco/markdown-preview.nvim/issues/690
+  build = ":call mkdp#util#install()",
   keys = {
     { "<leader>mp", ":MarkdownPreviewToggle<cr>", desc = "MarkdownPreview Toggle" },
   },
