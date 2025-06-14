71b0cd3b98510d0681a50ddf5c679e7c1bf550ef
Ivan Duran <idurandiaz@gmail.com>
2025-06-14 11:23:48 +0300

    feat: add Broot via vim-floaterm

---
+++ lua/plugins/vim-floaterm.lua
@@ -0,0 +1,66 @@
+return {
+  "voldikss/vim-floaterm",
+  cmd = "FloatermNew",
+  keys = {
+    { ",b", "<cmd>Broot<cr>", desc = "Broot (CWD)" },
+    { ",,", "<cmd>BrootCWD<cr>", desc = "Broot" },
+    { ",v", "<cmd>BrootSplit<cr>", desc = "Broot (Split)" },
+    { ",r", "<cmd>BrootSearch<cr>", desc = "Broot (Sarch Content)" },
+  },
+  config = function()
+    vim.g.floaterm_opener = "edit"
+    vim.g.floaterm_borderchars = "─│─│╭╮╯╰"
+
+    local win_width = 0.85
+    local win_height = 0.9
+
+    -- Helper function to create broot command with current file
+    local function get_broot_command(opts)
+      opts = opts or { cwd = false }
+      local current_file = vim.fn.expand("%:p")
+      local current_file2 = vim.fn.fnamemodify(vim.fn.expand("%"), ":~:.") -- file name with relative path
+      local broot_command = "broot -h"
+
+      -- Only add the file path if we're in a valid buffer
+      if current_file ~= "" then
+        broot_command = broot_command .. ' -c "' .. current_file2:gsub("/", "\\/") .. (opts.cwd and ':escape"' or '"')
+      end
+
+      return broot_command
+    end
+
+    -- Floating window (current file)
+    vim.api.nvim_create_user_command("Broot", function()
+      vim.cmd(
+        string.format("FloatermNew --width=%s --height=%s --title=Broot ", win_width, win_height) .. get_broot_command()
+      )
+    end, {})
+
+    -- Split terminal version (cwd)
+    vim.api.nvim_create_user_command("BrootSplit", function()
+      vim.cmd(
+        "FloatermNew --wintype=vsplit --width=0.32 --position=leftabove --title=Broot "
+          .. get_broot_command({ cwd = true })
+      )
+    end, {})
+
+    -- Floating window (cwd)
+    vim.api.nvim_create_user_command("BrootCWD", function()
+      vim.cmd(
+        string.format("FloatermNew --width=%s --height=%s --title=Broot ", win_width, win_height)
+          .. get_broot_command({ cwd = true })
+      )
+    end, {})
+
+    -- Floating window (Search Content)
+    vim.api.nvim_create_user_command("BrootSearch", function()
+      vim.cmd(
+        string.format(
+          'FloatermNew --width=%s --height=%s --title=Broot broot -h -c ":mode_input;c/"',
+          win_width,
+          win_height
+        )
+      )
+    end, {})
+  end,
+}
