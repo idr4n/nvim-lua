7c3d720beff8a80aa1197195607f68c5a14a5755
Ivan Duran <idurandiaz@gmail.com>
2025-06-17 12:27:29 +0300

    feat: add syntax highlight for tags in md files

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -9,4 +9,27 @@ return {
     },
     latex = { enabled = false },
   },
+  config = function(_, opts)
+    local bg_color = vim.api.nvim_get_hl(0, { name = "RenderMarkdownH6Bg" }).bg
+    local fg_color = vim.api.nvim_get_hl(0, { name = "RenderMarkdownH6Fg" }).fg
+
+    -- Setup hashtag syntax highlighting specifically for markdown files
+    local augroup = vim.api.nvim_create_augroup("MarkdownHashTag", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "FileType" }, {
+      group = augroup,
+      pattern = "*.md",
+      callback = function()
+        if vim.bo.filetype == "markdown" then
+          pcall(function()
+            vim.api.nvim_set_hl(0, "HashTag", { bg = bg_color, fg = fg_color, bold = true })
+          end)
+
+          -- Pattern that only matches standalone hashtags. This avoids things like variable#property
+          vim.cmd([[syntax match HashTag /\(^\|\s\)\@<=#\w\+/ contains=@NoSpell containedin=ALL]])
+        end
+      end,
+    })
+
+    require("render-markdown").setup(opts)
+  end,
 }
