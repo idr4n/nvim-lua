e9be67a1b5c0754dd449967b06d91b0a7994a6e0
Ivan Duran <idurandiaz@gmail.com>
2025-07-03 21:21:19 +0300

    feat: add aucmd on vimenter for netrw-preview

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -135,6 +135,18 @@ aucmd("FileType", {
 -- })
 --: }}}
 
+-- netrw-preview open if directory
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 1 then
+      local stat = vim.loop.fs_stat(vim.fn.argv(0))
+      if stat and stat.type == "directory" then
+        require("netrw-preview").reveal()
+      end
+    end
+  end,
+})
+
 -- Redraw statusline on different events
 aucmd("DiagnosticChanged", {
   group = augroup("Status_Diagnostics"),
