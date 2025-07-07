f39271f16ff2806cb159e8b16afc4eaf612ce825
Ivan Duran <idurandiaz@gmail.com>
2025-07-07 23:06:12 +0300

    feat: add gruvbox-material colorscheme

---
+++ lua/plugins/gruvbox-material.lua
@@ -0,0 +1,14 @@
+return {
+  "sainnhe/gruvbox-material",
+  priority = 1000,
+  config = function()
+    vim.g.gruvbox_material_transparent_background = 0
+    vim.g.gruvbox_material_foreground = "material"
+    vim.g.gruvbox_material_enable_bold = 1
+    vim.g.gruvbox_material_background = "hard"
+    vim.g.gruvbox_material_ui_contrast = "high"
+    vim.g.gruvbox_material_enable_italic = false
+    vim.g.gruvbox_material_disable_italic_comment = true
+    vim.g.netrw_special_syntax = true
+  end,
+}
