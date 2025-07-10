c51b7ddc558ba2381dc677f29624e6f2b8d9c523
Ivan Duran <idurandiaz@gmail.com>
2025-07-11 01:30:15 +0300

    refactor: signature help and hover lsp functions

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -36,7 +36,7 @@ return {
         ["vim.lsp.util.stylize_markdown"] = true,
         ["cmp.entry.get_documentation"] = true,
       },
-      hover = { enabled = true },
+      hover = { enabled = true, silent = true },
       signature = { enabled = true },
       progress = { enabled = false },
     },
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -116,16 +116,23 @@ return {
 
     -- If not using noice.nvim, configure hover and signatureHelp
     if not package.loaded["noice"] then
-      if vim.fn.has("nvim-0.10.0") == 1 then
-        vim.lsp.handlers[methods.textDocument_hover] = lsp_conf.enhanced_float_handler(vim.lsp.buf.hover, true)
-        vim.lsp.handlers[methods.textDocument_signatureHelp] =
-          lsp_conf.enhanced_float_handler(vim.lsp.buf.signature_help, false)
-      else
-        vim.lsp.handlers["textDocument/hover"] = vim.lsp.buf.with(vim.lsp.buf.hover, {
+      local hover = vim.lsp.buf.hover
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.lsp.buf.hover = function()
+        return hover({
           border = "rounded",
+          max_height = math.floor(vim.o.lines * 0.5),
+          max_width = math.floor(vim.o.columns * 0.5),
         })
-        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.buf.with(vim.lsp.buf.signature_help, {
+      end
+
+      local signature_help = vim.lsp.buf.signature_help
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.lsp.buf.signature_help = function()
+        return signature_help({
           border = "rounded",
+          max_height = math.floor(vim.o.lines * 0.5),
+          max_width = math.floor(vim.o.columns * 0.4),
         })
       end
     end
