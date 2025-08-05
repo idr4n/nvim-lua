b391d95b6591b4f38f0cfd3498fa5ca0879be143
Ivan Duran <idurandiaz@gmail.com>
2025-08-05 18:36:39 +0300

    refactor: remove plugins

--- lua/plugins/diffview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "sindrets/diffview.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = true,
-  keys = {
-    { "<leader>vo", ":DiffviewOpen<cr>", desc = "Diffview Project Open" },
-    { "<leader>vH", ":DiffviewFileHistory %<cr>", desc = "Diffview File History" },
-  },
-}
