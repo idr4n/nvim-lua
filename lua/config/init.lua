a13130824529bfce5ba23ab4dc5fac552de3d525
Ivan Duran <idurandiaz@gmail.com>
2026-01-18 21:41:29 +0300

    feat: use idr4n/andromeda colorscheme

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -71,4 +71,4 @@ require("lazy").setup("plugins", {
 })
 
 -- Load colorscheme
-vim.cmd("colorscheme tokyonight")
+vim.cmd("colorscheme andromeda")
---
+++ lua/plugins/andromeda.lua
@@ -0,0 +1,12 @@
+return {
+  "idr4n/andromeda.nvim",
+  -- enabled = false,
+  lazy = true,
+  priority = 1000,
+  opts = {
+    styles = {
+      comments = { italic = false },
+      keywords = { italic = false },
+    },
+  },
+}
